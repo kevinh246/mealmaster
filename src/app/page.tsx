43fedf0e272ad0fc890942f41bfe1c42b84b4714
Kevin <kehoward001@mymail.sim.edu.sg>
2024-07-25 10:54:11 +0800

    Updated landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
+import Link from "next/link";
+
+export default async function Home() {
+
   return (
-    <main>
-      Landing page
-    </main>
+    <div className="text-center mt-[20vh]">
+      <h1 className="text-4xl font-bold font-sans block">
+        Welcome to Mealmaster 🎉
+      </h1>
+      <p className="mt-3">
+        Get started on your journey towards a healthier meal
+      </p>
+      <img 
+        src="/assets/imgs/landing.png" 
+        width={300}
+        className="mx-auto"
+        alt="Landing image" 
+      />
+      <Link 
+        href="/dashboard"
+        className="btn bg-[#59a2f7] hover:bg-[#59a2f7] text-white border-none mt-3 px-20 rounded-full"
+      >
+        Start my journey
+      </Link>
+    </div>
   );
 }
