511704feee3f8fe96fb4249f1f56162cca76b51d
kjingtat <kjingtat@gmail.com>
2024-09-04 15:34:11 +0800

    Created not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import CenterContainer from "@/components/CenterContainer";
+
+
+export default function NotFound() {
+ return (
+   <CenterContainer>
+     <h1 className="text-2xl font-sans">
+       Error 404 | Page not found
+     </h1>
+   </CenterContainer>
+ )
+}
+
+
+
