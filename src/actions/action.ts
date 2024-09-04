cd9d306271a2656ab502c280a71c7979d2664b9c
Kirby <kltrloh001@mymail.sim.edu.sg>
2024-09-04 12:15:36 +0800

    Set up onboarding flow

---
+++ src/actions/action.ts
@@ -0,0 +1,65 @@
+"use server";
+
+import { User, currentUser } from "@clerk/nextjs/server";
+import { PrismaClient } from "@prisma/client";
+// import { redirect } from "next/navigation";
+
+const prisma = new PrismaClient();
+
+export async function completeOnboarding(gender: string, weight: number, age: number, active: string) {
+   try {
+       const user: User | null = await currentUser();
+
+       if (user?.id) {
+           await prisma.userAccount.update({
+               where: {
+                   id: user.id,
+               },
+               data: { gender, weight, age, active, onboardingCompleted: true },
+           });
+       }
+       else {
+           throw new Error("Error: user id is invalid");
+       }
+
+       const response: Response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/calorie`, {
+           method: "POST",
+           headers: {
+               "Content-Type": "application/json",
+           },
+           body: JSON.stringify({
+               prompt: `${gender}, ${age} years old, ${weight}kg, ${active}`
+           }),
+       });
+
+       if (!response.ok) {
+           throw new Error("eailed to fetch recommended calorie intake.");
+       }
+
+       const recommendedCalorieIntake = await response.json();
+       const calorieIntake = JSON.parse(recommendedCalorieIntake.message).recommendedCalorieIntake;
+
+       // Update the user's recommended calorie intake in the database
+       await prisma.userAccount.update({
+           where: {
+               id: user.id,
+           },
+           data: {
+               recommendedCalorieIntake: calorieIntake
+           },
+       });
+
+       // DO NOT do this as it will trigger a NEXT_REDIRECT error
+       // redirect("/dashboard");
+   }
+   catch (error: any) {
+       console.error("Error completing onboarding: ", error);
+   }
+   finally {
+       // Disconnect from Prisma client
+       await prisma.$disconnect();
+   }
+}
+
+
+
