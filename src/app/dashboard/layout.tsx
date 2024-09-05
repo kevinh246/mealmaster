f8004f5ab0f6562a18063a00afab966e4bcfe716
Kirby <ktrloh001@mymail.sim.edu.sg>
2024-09-05 16:00:53 +0800

    Add / remove blank spacing

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,8 +2,9 @@ import Container from "@/components/Container"
 import React from "react"
 import { currentUser } from '@clerk/nextjs/server';
 import Navbar from "@/components/Navbar";
+
 export default async function DashboardLayout({ children } : { children: React.ReactNode }) {
-  
+
   const user = await currentUser();
 
   return (
